368395af350e3dd298597186e39d873a5c7b0e23
Mike Collins <mike.d.collins@gmail.com>
2018-04-14 12:29:44 -0700

    oops

--- src/main/java/com/mike/website3/db/LoginNameExists.java
+++ src/main/java/com/mike/website3/db/LoginNameExists.java
@@ -1,4 +1,7 @@
 package com.mike.website3.db;
 
-public class LoginNameExists {
+public class LoginNameExists extends Exception {
+    public LoginNameExists(String loginName) {
+        super(loginName);
+    }
 }
