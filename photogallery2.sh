110a68df855ade075a29e23a4e8c90bb52fdc42d
Mike Collins <mike.d.collins@gmail.com>
2018-04-20 13:23:22 -0700

    handle mounting the extra disk

--- photogallery2.sh
+++ photogallery2.sh
@@ -3,6 +3,16 @@
 #cd /home/mike/photoGallery/photos/minecraft
 #java -jar minecraft_server.jar nogui &
 
-cd /home/photo/website
+# sudo lsblk
+# will show the available extra disks
 
+# get our extra big disk mounted, this is where
+# uploads are stored, this is safe to do multiple times (I think)
+#
+cd /home/photo/static
+sudo mount /dev/sdc users
+
+# start the server
+#
+cd /home/photo/website
 java -jar website.jar --delayStartup
