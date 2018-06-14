e1ba9f71b96c7710eccceb0dd2ba84e6cbe97f2a
Mike <mike.d.collins@gmail.com>
2018-06-14 19:05:54 +0000

    add minecraft server to startup

--- photogallery2.sh
+++ photogallery2.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#cd /home/mike/photoGallery/photos/minecraft
-#java -jar minecraft_server.jar nogui &
-
 # sudo lsblk
 # will show the available extra disks
 
 # get our extra big disk mounted, this is where
 # uploads are stored, this is safe to do multiple times (I think)
 #
-# this doesn't work, do it in the VM startup script
-#
-#sudo mount /dev/sdc /home/photo/static/users
+# the big disk must mount here /dev/sdc /home/photo/static/users
 
-# start the server
+# run a minecraft server for the guys, in the background
+#
+cd /home/photo/static/users/minecraft
+java -jar minecraft_server.1.12.2.jar  nogui &
+
+# start the photo server
 #
 cd /home/photo/website
 java -jar website.jar --delayStartup
